528feb0e1a871032f4ad6f11bfa9b9ffca117414
Vijay11945 <120110419+Vijay11945@users.noreply.github.com>
2022-12-17 20:35:53 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,9 @@
+console.log(document);
+console.log(document.domain);
+console.log(document.url);
+headertitle.innerHTML = "<h3>Hello</h3>
+var items = document.getElementClassName('list-group-item');
+console.log(items);
+console.log(items[i]);
+items.style.foreweight = "bold";
+items.style.backgroundcolour = "green";
