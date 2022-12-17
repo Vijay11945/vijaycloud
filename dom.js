dfd26dcff58f7d667b037c8224442d404f408f95
Vijay11945 <120110419+Vijay11945@users.noreply.github.com>
2022-12-17 21:05:09 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -7,3 +7,15 @@ console.log(items);
 console.log(items[i]);
 items.style.foreweight = "bold";
 items.style.backgroundcolour = "green";
+
+var titles = document.querySelectorAll
+var odd = document.query.SelectorAll('li:html.child(odd)');
+var even = document.query.SelectorAll('li:html.child(even)');
+for (var i = 0 ; i < add.length ; i++)
+{
+  odd[i].Style.backgroundcolour = 'green';
+  
+  even[i].Style.backgroundcolour = 'invisible';
+}
+
+
